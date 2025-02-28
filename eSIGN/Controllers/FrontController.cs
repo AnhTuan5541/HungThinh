cc5a65804be117becc15e7e41af0bfe0314a7efd
Nguyen Duc Tuan <anhtuan12296@gmail.com>
2025-02-28 17:02:30 +0700

    update some feature

--- eSIGN/Controllers/FrontController.cs
+++ eSIGN/Controllers/FrontController.cs
@@ -106,6 +106,49 @@ namespace HungThinh.Controllers
             }
         }
         [HttpGet]
+        public IActionResult GetLinhVucChiTiet(int id)
+        {
+            string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
+            string userid = "anonymous";
+            try
+            {
+                using var connection = new SqlConnection(_connection.DefaultConnection);
+                using var command = new SqlCommand("HT_GetLinhVucChiTiet", connection) { CommandType = CommandType.StoredProcedure };
+
+                // Thêm các tham số cho stored procedure (nếu cần)
+                command.Parameters.AddWithValue("@id", id);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                List<Dictionary<string, object>> data = CommonFunction.GetDataFromProcedure(reader);
+                connection.Close();
+
+                string resultMessage = "Get lĩnh vực chi tiết successfully!";
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
+                var response = new CommonResponse<Dictionary<string, object>>
+                {
+                    StatusCode = CommonFunction.SUCCESS,
+                    Message = resultMessage,
+                    Data = data,
+                    size = data.Count
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi (ví dụ: log lỗi, trả về phản hồi lỗi)
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, ex.Message, CommonFunction.ERROR, functionName);
+                var errorResponse = new CommonResponse<User>
+                {
+                    StatusCode = CommonFunction.ERROR,
+                    Message = ex.Message,
+                    Data = null,
+                    size = 0
+                };
+                return StatusCode(500, errorResponse);
+            }
+        }
+        [HttpGet]
         public IActionResult GetNangLuc()
         {
             string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
@@ -192,6 +235,49 @@ namespace HungThinh.Controllers
             }
         }
         [HttpGet]
+        public IActionResult GetProjectChiTiet(int id)
+        {
+            string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
+            string userid = "anonymous";
+            try
+            {
+                using var connection = new SqlConnection(_connection.DefaultConnection);
+                using var command = new SqlCommand("HT_GetProjectChiTiet", connection) { CommandType = CommandType.StoredProcedure };
+
+                // Thêm các tham số cho stored procedure (nếu cần)
+                command.Parameters.AddWithValue("@id", id);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                List<Dictionary<string, object>> data = CommonFunction.GetDataFromProcedure(reader);
+                connection.Close();
+
+                string resultMessage = "Get dự án chi tiết successfully!";
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
+                var response = new CommonResponse<Dictionary<string, object>>
+                {
+                    StatusCode = CommonFunction.SUCCESS,
+                    Message = resultMessage,
+                    Data = data,
+                    size = data.Count
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi (ví dụ: log lỗi, trả về phản hồi lỗi)
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, ex.Message, CommonFunction.ERROR, functionName);
+                var errorResponse = new CommonResponse<User>
+                {
+                    StatusCode = CommonFunction.ERROR,
+                    Message = ex.Message,
+                    Data = null,
+                    size = 0
+                };
+                return StatusCode(500, errorResponse);
+            }
+        }
+        [HttpGet]
         public IActionResult GetTinTuc()
         {
             string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
@@ -210,6 +296,49 @@ namespace HungThinh.Controllers
                 connection.Close();
 
                 string resultMessage = "Get tin tức successfully!";
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
+                var response = new CommonResponse<Dictionary<string, object>>
+                {
+                    StatusCode = CommonFunction.SUCCESS,
+                    Message = resultMessage,
+                    Data = data,
+                    size = data.Count
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi (ví dụ: log lỗi, trả về phản hồi lỗi)
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, ex.Message, CommonFunction.ERROR, functionName);
+                var errorResponse = new CommonResponse<User>
+                {
+                    StatusCode = CommonFunction.ERROR,
+                    Message = ex.Message,
+                    Data = null,
+                    size = 0
+                };
+                return StatusCode(500, errorResponse);
+            }
+        }
+        [HttpGet]
+        public IActionResult GetTinTucChiTiet(int id)
+        {
+            string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
+            string userid = "anonymous";
+            try
+            {
+                using var connection = new SqlConnection(_connection.DefaultConnection);
+                using var command = new SqlCommand("HT_GetTinTucChiTiet", connection) { CommandType = CommandType.StoredProcedure };
+
+                // Thêm các tham số cho stored procedure (nếu cần)
+                command.Parameters.AddWithValue("@id", id);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                List<Dictionary<string, object>> data = CommonFunction.GetDataFromProcedure(reader);
+                connection.Close();
+
+                string resultMessage = "Get tin tức chi tiết successfully!";
                 CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
                 var response = new CommonResponse<Dictionary<string, object>>
                 {
---
+++ eSIGN/Controllers/LienHeController.cs
@@ -0,0 +1,119 @@
+﻿using HungThinh.Common;
+using HungThinh.Data;
+using HungThinh.Response;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using System.Data;
+using System.Security.Claims;
+
+namespace HungThinh.Controllers
+{
+    public class LienHeController : Controller
+    {
+        private readonly ILogger<LienHeController> _logger;
+        private readonly ConnectionStrings _connection;
+        public LienHeController(ILogger<LienHeController> logger, IOptions<ConnectionStrings> connection)
+        {
+            _logger = logger;
+            _connection = connection.Value;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetLienHe()
+        {
+            string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
+            string userid = User.FindFirstValue(ClaimTypes.Name);
+            try
+            {
+                using var connection = new SqlConnection(_connection.DefaultConnection);
+                using var command = new SqlCommand("HT_GetLienHe", connection) { CommandType = CommandType.StoredProcedure };
+
+                // Thêm các tham số cho stored procedure (nếu cần)
+                //command.Parameters.AddWithValue("@role", role);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                List<Dictionary<string, object>> data = CommonFunction.GetDataFromProcedure(reader);
+                connection.Close();
+
+                string resultMessage = "Get liên hệ successfully!";
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
+                var response = new CommonResponse<Dictionary<string, object>>
+                {
+                    StatusCode = CommonFunction.SUCCESS,
+                    Message = resultMessage,
+                    Data = data,
+                    size = data.Count
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi (ví dụ: log lỗi, trả về phản hồi lỗi)
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, ex.Message, CommonFunction.ERROR, functionName);
+                var errorResponse = new CommonResponse<User>
+                {
+                    StatusCode = CommonFunction.ERROR,
+                    Message = ex.Message,
+                    Data = null,
+                    size = 0
+                };
+                return StatusCode(500, errorResponse);
+            }
+        }
+        [HttpPost]
+        [Authorize]
+        public IActionResult UpdateLienHe(int id, string dia_chi, string dien_thoai, string email, string dia_chi_google_map)
+        {
+            string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
+            string userid = User.FindFirstValue(ClaimTypes.Name);
+            try
+            {
+                using var connection = new SqlConnection(_connection.DefaultConnection);
+                using var command = new SqlCommand("HT_UpdateLienHe", connection) { CommandType = CommandType.StoredProcedure };
+
+                // Thêm các tham số cho stored procedure (nếu cần)
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@dia_chi", dia_chi == null ? "": dia_chi);
+                command.Parameters.AddWithValue("@dien_thoai", dien_thoai == null ? "" : dien_thoai);
+                command.Parameters.AddWithValue("@email", email == null ? "" : email);
+                command.Parameters.AddWithValue("@dia_chi_google_map", dia_chi_google_map == null ? "" : dia_chi_google_map);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                List<Dictionary<string, object>> data = CommonFunction.GetDataFromProcedure(reader);
+                connection.Close();
+
+                string resultMessage = "Cập nhật liên hệ thành công!";
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
+                var response = new CommonResponse<Dictionary<string, object>>
+                {
+                    StatusCode = CommonFunction.SUCCESS,
+                    Message = resultMessage,
+                    Data = data,
+                    size = data.Count
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi (ví dụ: log lỗi, trả về phản hồi lỗi)
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, ex.Message, CommonFunction.ERROR, functionName);
+                var errorResponse = new CommonResponse<User>
+                {
+                    StatusCode = CommonFunction.ERROR,
+                    Message = ex.Message,
+                    Data = null,
+                    size = 0
+                };
+                return StatusCode(500, errorResponse);
+            }
+        }
+    }
+}

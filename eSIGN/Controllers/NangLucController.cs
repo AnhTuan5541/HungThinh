6ca6b32fbe41ef1e29c798ef5632de8f95da4af6
Nguyen Duc Tuan <anhtuan12296@gmail.com>
2025-03-19 16:20:10 +0700

    update nang luc

--- eSIGN/Controllers/NangLucController.cs
+++ eSIGN/Controllers/NangLucController.cs
@@ -69,6 +69,141 @@ namespace HungThinh.Controllers
         }
         [HttpGet]
         [Authorize]
+        public IActionResult GetNangLucTaiSaoNenChon()
+        {
+            string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
+            string userid = User.FindFirstValue(ClaimTypes.Name);
+            try
+            {
+                using var connection = new SqlConnection(_connection.DefaultConnection);
+                using var command = new SqlCommand("HT_GetNangLucTaiSaoNenChon", connection) { CommandType = CommandType.StoredProcedure };
+
+                // Thêm các tham số cho stored procedure (nếu cần)
+                //command.Parameters.AddWithValue("@role", role);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                List<Dictionary<string, object>> data = CommonFunction.GetDataFromProcedure(reader);
+                connection.Close();
+
+                string resultMessage = "Get năng lực tại sao nên chọn successfully!";
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
+                var response = new CommonResponse<Dictionary<string, object>>
+                {
+                    StatusCode = CommonFunction.SUCCESS,
+                    Message = resultMessage,
+                    Data = data,
+                    size = data.Count
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi (ví dụ: log lỗi, trả về phản hồi lỗi)
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, ex.Message, CommonFunction.ERROR, functionName);
+                var errorResponse = new CommonResponse<User>
+                {
+                    StatusCode = CommonFunction.ERROR,
+                    Message = ex.Message,
+                    Data = null,
+                    size = 0
+                };
+                return StatusCode(500, errorResponse);
+            }
+        }
+        [HttpPost]
+        [Authorize]
+        public IActionResult UpdateFileTaiSaoNenChon(int id, string detail, string file_url, string file_name)
+        {
+            string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
+            string userid = User.FindFirstValue(ClaimTypes.Name);
+            try
+            {
+                using var connection = new SqlConnection(_connection.DefaultConnection);
+                using var command = new SqlCommand("HT_UpdateTaiSaoNenChon", connection) { CommandType = CommandType.StoredProcedure };
+
+                // Thêm các tham số cho stored procedure (nếu cần)
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@detail", detail == null ? "" : detail);
+                command.Parameters.AddWithValue("@file_url", file_url == null ? "" : file_url);
+                command.Parameters.AddWithValue("@file_name", file_name == null ? "" : file_name);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                List<Dictionary<string, object>> data = CommonFunction.GetDataFromProcedure(reader);
+                connection.Close();
+
+                string resultMessage = "Cập nhật tại sao nên chọn chúng tôi thành công!";
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
+                var response = new CommonResponse<Dictionary<string, object>>
+                {
+                    StatusCode = CommonFunction.SUCCESS,
+                    Message = resultMessage,
+                    Data = data,
+                    size = data.Count
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi (ví dụ: log lỗi, trả về phản hồi lỗi)
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, ex.Message, CommonFunction.ERROR, functionName);
+                var errorResponse = new CommonResponse<User>
+                {
+                    StatusCode = CommonFunction.ERROR,
+                    Message = ex.Message,
+                    Data = null,
+                    size = 0
+                };
+                return StatusCode(500, errorResponse);
+            }
+        }
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetNangLucTaiSaoNenChonContent()
+        {
+            string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
+            string userid = User.FindFirstValue(ClaimTypes.Name);
+            try
+            {
+                using var connection = new SqlConnection(_connection.DefaultConnection);
+                using var command = new SqlCommand("HT_GetNangLucTaiSaoNenChonContent", connection) { CommandType = CommandType.StoredProcedure };
+
+                // Thêm các tham số cho stored procedure (nếu cần)
+                //command.Parameters.AddWithValue("@role", role);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                List<Dictionary<string, object>> data = CommonFunction.GetDataFromProcedure(reader);
+                connection.Close();
+
+                string resultMessage = "Get năng lực tại sao nên chọn content successfully!";
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
+                var response = new CommonResponse<Dictionary<string, object>>
+                {
+                    StatusCode = CommonFunction.SUCCESS,
+                    Message = resultMessage,
+                    Data = data,
+                    size = data.Count
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi (ví dụ: log lỗi, trả về phản hồi lỗi)
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, ex.Message, CommonFunction.ERROR, functionName);
+                var errorResponse = new CommonResponse<User>
+                {
+                    StatusCode = CommonFunction.ERROR,
+                    Message = ex.Message,
+                    Data = null,
+                    size = 0
+                };
+                return StatusCode(500, errorResponse);
+            }
+        }
+        [HttpGet]
+        [Authorize]
         public IActionResult GetHoSoNangLuc()
         {
             string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
@@ -111,52 +246,7 @@ namespace HungThinh.Controllers
                 return StatusCode(500, errorResponse);
             }
         }
-        //[HttpPost]
-        //[Authorize]
-        //public IActionResult UpdateHSNangLuc(string file_url, string file_name)
-        //{
-        //    string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
-        //    string userid = User.FindFirstValue(ClaimTypes.Name);
-        //    try
-        //    {
-        //        using var connection = new SqlConnection(_connection.DefaultConnection);
-        //        using var command = new SqlCommand("HT_UpdateHoSoNangLuc", connection) { CommandType = CommandType.StoredProcedure };
-
-        //        // Thêm các tham số cho stored procedure (nếu cần)
-        //        command.Parameters.AddWithValue("@id", 1);
-        //        command.Parameters.AddWithValue("@file_url", file_url == null ? "" : file_url);
-        //        command.Parameters.AddWithValue("@file_name", file_name == null ? "" : file_name);
-
-        //        connection.Open();
-        //        var reader = command.ExecuteReader();
-        //        List<Dictionary<string, object>> data = CommonFunction.GetDataFromProcedure(reader);
-        //        connection.Close();
-
-        //        string resultMessage = "Update hồ sơ năng lực thành công!";
-        //        CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
-        //        var response = new CommonResponse<Dictionary<string, object>>
-        //        {
-        //            StatusCode = CommonFunction.SUCCESS,
-        //            Message = resultMessage,
-        //            Data = data,
-        //            size = data.Count
-        //        };
-        //        return Ok(response);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Xử lý lỗi (ví dụ: log lỗi, trả về phản hồi lỗi)
-        //        CommonFunction.LogInfo(_connection.DefaultConnection, userid, ex.Message, CommonFunction.ERROR, functionName);
-        //        var errorResponse = new CommonResponse<User>
-        //        {
-        //            StatusCode = CommonFunction.ERROR,
-        //            Message = ex.Message,
-        //            Data = null,
-        //            size = 0
-        //        };
-        //        return StatusCode(500, errorResponse);
-        //    }
-        //}
+        
         [HttpPost]
         [Authorize]
         public async Task<IActionResult> UploadFile(IFormFile file)
@@ -220,6 +310,52 @@ namespace HungThinh.Controllers
                 connection.Close();
 
                 string resultMessage = "Cập nhật năng lực thành công!";
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
+                var response = new CommonResponse<Dictionary<string, object>>
+                {
+                    StatusCode = CommonFunction.SUCCESS,
+                    Message = resultMessage,
+                    Data = data,
+                    size = data.Count
+                };
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi (ví dụ: log lỗi, trả về phản hồi lỗi)
+                CommonFunction.LogInfo(_connection.DefaultConnection, userid, ex.Message, CommonFunction.ERROR, functionName);
+                var errorResponse = new CommonResponse<User>
+                {
+                    StatusCode = CommonFunction.ERROR,
+                    Message = ex.Message,
+                    Data = null,
+                    size = 0
+                };
+                return StatusCode(500, errorResponse);
+            }
+        }
+        [HttpPost]
+        [Authorize]
+        public IActionResult UpdateNangLucTaiSaoNenChonContent(int id, string title, string detail)
+        {
+            string functionName = ControllerContext.ActionDescriptor.ControllerName + "/" + System.Reflection.MethodBase.GetCurrentMethod().Name;
+            string userid = User.FindFirstValue(ClaimTypes.Name);
+            try
+            {
+                using var connection = new SqlConnection(_connection.DefaultConnection);
+                using var command = new SqlCommand("HT_UpdateNangLucTaiSaoNenChonContent", connection) { CommandType = CommandType.StoredProcedure };
+
+                // Thêm các tham số cho stored procedure (nếu cần)
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.AddWithValue("@title", title == null ? "" : title);
+                command.Parameters.AddWithValue("@detail", detail == null ? "" : detail);
+
+                connection.Open();
+                var reader = command.ExecuteReader();
+                List<Dictionary<string, object>> data = CommonFunction.GetDataFromProcedure(reader);
+                connection.Close();
+
+                string resultMessage = "Cập nhật tiêu chí thành công!";
                 CommonFunction.LogInfo(_connection.DefaultConnection, userid, resultMessage, CommonFunction.SUCCESS, functionName);
                 var response = new CommonResponse<Dictionary<string, object>>
                 {
